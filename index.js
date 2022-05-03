468c0016d3f6a501c8ac8a43a3baf8d05d662359
Funny Rose <fannyperretlombard@gmail.com>
2022-05-03 10:16:08 +0200

    Add final touch

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+let showModal = function () {
+
+    var modal = document.getElementById("congratulationModal");
+    var badge = document.querySelector(".badge");
+
+    modal.style.display = "flex";
+    badge.style.backgroundColor = "#181A21";
+
+}
+
+
 let startTimer = function (duration, display) {
 
     var timer = duration, minutes, seconds;
@@ -5,8 +16,6 @@ let startTimer = function (duration, display) {
     setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-
-        console.log(seconds);
 
         minutes = minutes < 10 ? + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -24,7 +33,51 @@ window.onload = function () {
     var fiveMinutes = 60 * 5 + 2,
         display = document.querySelector('#counterTimer');
     startTimer(fiveMinutes, display);
+
+    setTimeout(() => {
+        giveAStar();
+        showModal();
+    }, 3000);
 };
 
+let giveAStar = function () {
 
-let 
+    let incrementStar = document.querySelector('#countingStars');
+    let value = incrementStar.textContent;
+    value = parseInt(value);
+
+    incrementStar.textContent = value + 1;
+
+}
+
+let toggleButton = function () {
+
+    var button = document.getElementById("toggleButton"),
+        buttonsGroup = document.querySelector(".icons-container")
+    ;
+
+    button.addEventListener('click', () => {
+
+        var iconArrow = document.querySelector(".arrow"),
+            arrowTop = "top"
+        ;
+
+        if (buttonsGroup.style.visibility === 'visible' || buttonsGroup.style.visibility === "") {
+            buttonsGroup.style.visibility = 'hidden'
+        } else {
+            buttonsGroup.style.visibility = 'visible';
+        }
+
+        if (iconArrow.src.includes(arrowTop)) {
+            iconArrow.src = "assets/icon-arrow-bottom.svg";
+        } else {
+            iconArrow.src = "assets/icon-arrow-top.svg";
+        }
+        
+    });
+
+}
+
+toggleButton();
+
+
